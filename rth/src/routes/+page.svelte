5e1d7bb26e113de12a2b7513c400343381b8690d
Elena Lungoci <simona.lungoci@yahoo.com>
2024-09-11 22:43:06 -0400

    initializing svelte, importing list of hanzi and dynamic routese for each hanzi

---
+++ rth/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import{Hanzi} from '$lib/Store'
+</script>
+  
+
+<h1>Welcome to RTH stories! </h1>
+<p>Get started here: {$Hanzi[999]}</p>
---
+++ rth/src/routes/hanzi/[slug]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<h1>Hanzi: {$page.params.slug}</h1>
+
